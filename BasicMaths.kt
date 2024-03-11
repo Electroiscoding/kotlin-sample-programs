cfcf585d5e25673f3fb3794a5424174d40147bd3
Electroiscoding <103299713+Electroiscoding@users.noreply.github.com>
2024-03-11 18:57:51 +0530

    BasicMaths.kt

---
+++ BasicMaths.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a = 10
+    val b = 5
+
+    println("Sum: " + (a + b))
+    println("Difference: " + (a - b))
+    println("Product: " + (a * b))
+    println("Quotient: " + (a / b))
+}
