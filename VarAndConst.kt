4f99a35ba4ddaf23288274120d9dd17cadd83d16
Electroiscoding <103299713+Electroiscoding@users.noreply.github.com>
2024-03-11 18:57:12 +0530

    VarAndConst.kt

---
+++ VarAndConst.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // Variables//
+    var name = "John"
+    var age = 25
+
+    // Constants//
+    val pi = 3.14
+
+    println("Name: " + name + ", Age: " + age)
+    println("PI: " + pi)
+}
